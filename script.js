3853faa3b5bb2272cc2dc7616c5c8af1e2db5192
gmejianeva78-byte <gmejianeva78@gmail.com>
2025-09-03 22:50:00 +0000

    script

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 console.log("hola");
 
 const titulo = document.getElementById("tituloPrincipal");
+let estadoOriginal = true;
 
-console.log(titulo);
+titulo.innerText = "Dos conejos con zanahoria"
+
+titulo.addEventListener("click", () => {
+    if(estadoOriginal === true) {
+        titulo.innerText = "UNA SERPIENTE MUTANTE";
+        titulo.style.color = "purple";
+    }
+
+else {
+    titulo.innerText = "Dos conejos con zanahoria";
+    titulo.style.color = "green";
+}
+estadoOriginal = !estadoOriginal;
+});
+
